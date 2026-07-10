941dff378261fc28d6f70c4d201f507db60a5e1f
campaign <campaign@thesis.local>
2026-07-10 17:06:40 +0100

    [013/risky] feat: large module gen_9 (light tests)

---
+++ app/gen_9.py
@@ -0,0 +1,187 @@
+"""Generated module 9 (campaign)."""
+
+
+def value_9(x):
+    """Return x scaled by 8 plus 3."""
+    return x * 8 + 3
+
+
+def helper_9_0(x):
+    return x + 0
+
+def helper_9_1(x):
+    return x + 1
+
+def helper_9_2(x):
+    return x + 2
+
+def helper_9_3(x):
+    return x + 3
+
+def helper_9_4(x):
+    return x + 4
+
+def helper_9_5(x):
+    return x + 5
+
+def helper_9_6(x):
+    return x + 6
+
+def helper_9_7(x):
+    return x + 7
+
+def helper_9_8(x):
+    return x + 8
+
+def helper_9_9(x):
+    return x + 9
+
+def helper_9_10(x):
+    return x + 10
+
+def helper_9_11(x):
+    return x + 11
+
+def helper_9_12(x):
+    return x + 12
+
+def helper_9_13(x):
+    return x + 13
+
+def helper_9_14(x):
+    return x + 14
+
+def helper_9_15(x):
+    return x + 15
+
+def helper_9_16(x):
+    return x + 16
+
+def helper_9_17(x):
+    return x + 17
+
+def helper_9_18(x):
+    return x + 18
+
+def helper_9_19(x):
+    return x + 19
+
+def helper_9_20(x):
+    return x + 20
+
+def helper_9_21(x):
+    return x + 21
+
+def helper_9_22(x):
+    return x + 22
+
+def helper_9_23(x):
+    return x + 23
+
+def helper_9_24(x):
+    return x + 24
+
+def helper_9_25(x):
+    return x + 25
+
+def helper_9_26(x):
+    return x + 26
+
+def helper_9_27(x):
+    return x + 27
+
+def helper_9_28(x):
+    return x + 28
+
+def helper_9_29(x):
+    return x + 29
+
+def helper_9_30(x):
+    return x + 30
+
+def helper_9_31(x):
+    return x + 31
+
+def helper_9_32(x):
+    return x + 32
+
+def helper_9_33(x):
+    return x + 33
+
+def helper_9_34(x):
+    return x + 34
+
+def helper_9_35(x):
+    return x + 35
+
+def helper_9_36(x):
+    return x + 36
+
+def helper_9_37(x):
+    return x + 37
+
+def helper_9_38(x):
+    return x + 38
+
+def helper_9_39(x):
+    return x + 39
+
+def helper_9_40(x):
+    return x + 40
+
+def helper_9_41(x):
+    return x + 41
+
+def helper_9_42(x):
+    return x + 42
+
+def helper_9_43(x):
+    return x + 43
+
+def helper_9_44(x):
+    return x + 44
+
+def helper_9_45(x):
+    return x + 45
+
+def helper_9_46(x):
+    return x + 46
+
+def helper_9_47(x):
+    return x + 47
+
+def helper_9_48(x):
+    return x + 48
+
+def helper_9_49(x):
+    return x + 49
+
+def helper_9_50(x):
+    return x + 50
+
+def helper_9_51(x):
+    return x + 51
+
+def helper_9_52(x):
+    return x + 52
+
+def helper_9_53(x):
+    return x + 53
+
+def helper_9_54(x):
+    return x + 54
+
+def helper_9_55(x):
+    return x + 55
+
+def helper_9_56(x):
+    return x + 56
+
+def helper_9_57(x):
+    return x + 57
+
+def helper_9_58(x):
+    return x + 58
+
+def helper_9_59(x):
+    return x + 59
