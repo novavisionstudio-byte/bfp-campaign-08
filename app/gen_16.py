74e0f5515cbd25c3b49032622cf8620bbe5cf876
campaign <campaign@thesis.local>
2026-07-10 17:18:34 +0100

    [023/risky] feat: large module gen_16 (light tests)

---
+++ app/gen_16.py
@@ -0,0 +1,169 @@
+"""Generated module 16 (campaign)."""
+
+
+def value_16(x):
+    """Return x scaled by 3 plus 1."""
+    return x * 3 + 1
+
+
+def helper_16_0(x):
+    return x + 0
+
+def helper_16_1(x):
+    return x + 1
+
+def helper_16_2(x):
+    return x + 2
+
+def helper_16_3(x):
+    return x + 3
+
+def helper_16_4(x):
+    return x + 4
+
+def helper_16_5(x):
+    return x + 5
+
+def helper_16_6(x):
+    return x + 6
+
+def helper_16_7(x):
+    return x + 7
+
+def helper_16_8(x):
+    return x + 8
+
+def helper_16_9(x):
+    return x + 9
+
+def helper_16_10(x):
+    return x + 10
+
+def helper_16_11(x):
+    return x + 11
+
+def helper_16_12(x):
+    return x + 12
+
+def helper_16_13(x):
+    return x + 13
+
+def helper_16_14(x):
+    return x + 14
+
+def helper_16_15(x):
+    return x + 15
+
+def helper_16_16(x):
+    return x + 16
+
+def helper_16_17(x):
+    return x + 17
+
+def helper_16_18(x):
+    return x + 18
+
+def helper_16_19(x):
+    return x + 19
+
+def helper_16_20(x):
+    return x + 20
+
+def helper_16_21(x):
+    return x + 21
+
+def helper_16_22(x):
+    return x + 22
+
+def helper_16_23(x):
+    return x + 23
+
+def helper_16_24(x):
+    return x + 24
+
+def helper_16_25(x):
+    return x + 25
+
+def helper_16_26(x):
+    return x + 26
+
+def helper_16_27(x):
+    return x + 27
+
+def helper_16_28(x):
+    return x + 28
+
+def helper_16_29(x):
+    return x + 29
+
+def helper_16_30(x):
+    return x + 30
+
+def helper_16_31(x):
+    return x + 31
+
+def helper_16_32(x):
+    return x + 32
+
+def helper_16_33(x):
+    return x + 33
+
+def helper_16_34(x):
+    return x + 34
+
+def helper_16_35(x):
+    return x + 35
+
+def helper_16_36(x):
+    return x + 36
+
+def helper_16_37(x):
+    return x + 37
+
+def helper_16_38(x):
+    return x + 38
+
+def helper_16_39(x):
+    return x + 39
+
+def helper_16_40(x):
+    return x + 40
+
+def helper_16_41(x):
+    return x + 41
+
+def helper_16_42(x):
+    return x + 42
+
+def helper_16_43(x):
+    return x + 43
+
+def helper_16_44(x):
+    return x + 44
+
+def helper_16_45(x):
+    return x + 45
+
+def helper_16_46(x):
+    return x + 46
+
+def helper_16_47(x):
+    return x + 47
+
+def helper_16_48(x):
+    return x + 48
+
+def helper_16_49(x):
+    return x + 49
+
+def helper_16_50(x):
+    return x + 50
+
+def helper_16_51(x):
+    return x + 51
+
+def helper_16_52(x):
+    return x + 52
+
+def helper_16_53(x):
+    return x + 53
