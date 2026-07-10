d3ef50886c1348c68ece143173f2783004df9047
campaign <campaign@thesis.local>
2026-07-10 17:05:12 +0100

    [012/risky] feat: large module gen_8 (light tests)

---
+++ app/gen_8.py
@@ -0,0 +1,109 @@
+"""Generated module 8 (campaign)."""
+
+
+def value_8(x):
+    """Return x scaled by 4 plus 5."""
+    return x * 4 + 5
+
+
+def helper_8_0(x):
+    return x + 0
+
+def helper_8_1(x):
+    return x + 1
+
+def helper_8_2(x):
+    return x + 2
+
+def helper_8_3(x):
+    return x + 3
+
+def helper_8_4(x):
+    return x + 4
+
+def helper_8_5(x):
+    return x + 5
+
+def helper_8_6(x):
+    return x + 6
+
+def helper_8_7(x):
+    return x + 7
+
+def helper_8_8(x):
+    return x + 8
+
+def helper_8_9(x):
+    return x + 9
+
+def helper_8_10(x):
+    return x + 10
+
+def helper_8_11(x):
+    return x + 11
+
+def helper_8_12(x):
+    return x + 12
+
+def helper_8_13(x):
+    return x + 13
+
+def helper_8_14(x):
+    return x + 14
+
+def helper_8_15(x):
+    return x + 15
+
+def helper_8_16(x):
+    return x + 16
+
+def helper_8_17(x):
+    return x + 17
+
+def helper_8_18(x):
+    return x + 18
+
+def helper_8_19(x):
+    return x + 19
+
+def helper_8_20(x):
+    return x + 20
+
+def helper_8_21(x):
+    return x + 21
+
+def helper_8_22(x):
+    return x + 22
+
+def helper_8_23(x):
+    return x + 23
+
+def helper_8_24(x):
+    return x + 24
+
+def helper_8_25(x):
+    return x + 25
+
+def helper_8_26(x):
+    return x + 26
+
+def helper_8_27(x):
+    return x + 27
+
+def helper_8_28(x):
+    return x + 28
+
+def helper_8_29(x):
+    return x + 29
+
+def helper_8_30(x):
+    return x + 30
+
+def helper_8_31(x):
+    return x + 31
+
+def helper_8_32(x):
+    return x + 32
+
+def helper_8_33(x):
+    return x + 33
