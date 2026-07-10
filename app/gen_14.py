34d8af4a4d8d544e2ef0e6fcaf0b21ce2af921d1
campaign <campaign@thesis.local>
2026-07-10 17:15:59 +0100

    [021/risky] feat: large module gen_14 (light tests)

---
+++ app/gen_14.py
@@ -0,0 +1,148 @@
+"""Generated module 14 (campaign)."""
+
+
+def value_14(x):
+    """Return x scaled by 2 plus 3."""
+    return x * 2 + 3
+
+
+def helper_14_0(x):
+    return x + 0
+
+def helper_14_1(x):
+    return x + 1
+
+def helper_14_2(x):
+    return x + 2
+
+def helper_14_3(x):
+    return x + 3
+
+def helper_14_4(x):
+    return x + 4
+
+def helper_14_5(x):
+    return x + 5
+
+def helper_14_6(x):
+    return x + 6
+
+def helper_14_7(x):
+    return x + 7
+
+def helper_14_8(x):
+    return x + 8
+
+def helper_14_9(x):
+    return x + 9
+
+def helper_14_10(x):
+    return x + 10
+
+def helper_14_11(x):
+    return x + 11
+
+def helper_14_12(x):
+    return x + 12
+
+def helper_14_13(x):
+    return x + 13
+
+def helper_14_14(x):
+    return x + 14
+
+def helper_14_15(x):
+    return x + 15
+
+def helper_14_16(x):
+    return x + 16
+
+def helper_14_17(x):
+    return x + 17
+
+def helper_14_18(x):
+    return x + 18
+
+def helper_14_19(x):
+    return x + 19
+
+def helper_14_20(x):
+    return x + 20
+
+def helper_14_21(x):
+    return x + 21
+
+def helper_14_22(x):
+    return x + 22
+
+def helper_14_23(x):
+    return x + 23
+
+def helper_14_24(x):
+    return x + 24
+
+def helper_14_25(x):
+    return x + 25
+
+def helper_14_26(x):
+    return x + 26
+
+def helper_14_27(x):
+    return x + 27
+
+def helper_14_28(x):
+    return x + 28
+
+def helper_14_29(x):
+    return x + 29
+
+def helper_14_30(x):
+    return x + 30
+
+def helper_14_31(x):
+    return x + 31
+
+def helper_14_32(x):
+    return x + 32
+
+def helper_14_33(x):
+    return x + 33
+
+def helper_14_34(x):
+    return x + 34
+
+def helper_14_35(x):
+    return x + 35
+
+def helper_14_36(x):
+    return x + 36
+
+def helper_14_37(x):
+    return x + 37
+
+def helper_14_38(x):
+    return x + 38
+
+def helper_14_39(x):
+    return x + 39
+
+def helper_14_40(x):
+    return x + 40
+
+def helper_14_41(x):
+    return x + 41
+
+def helper_14_42(x):
+    return x + 42
+
+def helper_14_43(x):
+    return x + 43
+
+def helper_14_44(x):
+    return x + 44
+
+def helper_14_45(x):
+    return x + 45
+
+def helper_14_46(x):
+    return x + 46
